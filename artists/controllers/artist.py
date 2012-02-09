4632bea091835444748a8f8a9050d80ca1a2a0bc
Mr. Petru <petrea.email@gmail.com>
2012-02-09 12:21:11 +0100

    implemented upload_file

--- artists/controllers/artist.py
+++ artists/controllers/artist.py
@@ -15,7 +15,7 @@ from artists.lib.base import BaseController
 from artists.model import *
 from artists.model.artist import * #Artist, Role
 from repoze.what import predicates
-from tg import redirect
+from tg import redirect, config, url
 #from artists.model import DeclarativeBase, metadata, DBSession
 
 from repoze.what.predicates import in_group
@@ -23,25 +23,30 @@ import tw2.core, tw2.forms
 import transaction
 from artists.lib import helpers
 
+import shutil
 
 class AddForm(tw2.forms.TableForm):
             action = '/artist/post_add'
             firstname = tw2.forms.TextField(label='NOME', validator=tw2.core.Required)
             lastname = tw2.forms.TextField(label='COGNOME', validator=tw2.core.Required)
-            sitelink = tw2.forms.TextField(label='SITE LINK', validator=tw2.core.Required, value='testssss')
-            reellink = tw2.forms.TextField(label='REEL LINK', validator=tw2.core.Required, value='testssss')
-            role = tw2.forms.TextField(label='RUOLO', validator=tw2.core.Required, value='testssss')
-            software = tw2.forms.TextField(label='SOFTWARE', validator=tw2.core.Required, value='testssss')
-            tags = tw2.forms.TextField(label='TAGS', validator=tw2.core.Required, value='testssss')
-            phone = tw2.forms.TextField(label='PHONE', validator=tw2.core.Required, value='testssss')
-            email = tw2.forms.TextField(label='EMAIL', validator=tw2.core.Required, value='testssss')
-            skype = tw2.forms.TextField(label='SKYPE', validator=tw2.core.Required, value='testssss')
-            othercontacts = tw2.forms.TextField(label='OTHER CONTACTS', validator=tw2.core.Required, value='testssss')
-            note = tw2.forms.TextArea(label='NOTE', validator=tw2.core.Required, value=' bla bla bla')
-            cvlocal = tw2.forms.LinkField(label='CV-local', link='http://petru.com', text='get it')
+            sitelink = tw2.forms.TextField(label='SITE LINK', validator=tw2.core.Required)
+            reellink = tw2.forms.TextField(label='REEL LINK', validator=tw2.core.Required)
+            role = tw2.forms.TextField(label='RUOLO', validator=tw2.core.Required)
+            software = tw2.forms.TextField(label='SOFTWARE', validator=tw2.core.Required)
+            tags = tw2.forms.TextField(label='TAGS', validator=tw2.core.Required)
+            phone = tw2.forms.TextField(label='PHONE', validator=tw2.core.Required)
+            email = tw2.forms.TextField(label='EMAIL', validator=tw2.core.Required)
+            skype = tw2.forms.TextField(label='SKYPE', validator=tw2.core.Required)
+            othercontacts = tw2.forms.TextField(label='OTHER CONTACTS', validator=tw2.core.Required)
+            note = tw2.forms.TextArea(label='NOTE', validator=tw2.core.Required)
+            cv_upload = tw2.forms.FileField()
             newartist = tw2.forms.RadioButtonList(label='NEW ARTIST', options=[[1, 'YES'],[0, 'NO']], default=1)
             
-add_form = AddForm(redirect='/artis/get_all').req()
+#add_form = AddForm(redirect='/artis/get_all').req()
+add_form = AddForm()
+
+class GetAllTble(tw2.forms.TableLayout):
+    pass
 
 class Controller(BaseController):
     #Uncomment this line if your controller requires an authenticated user
@@ -63,18 +68,28 @@ class Controller(BaseController):
         
         w = AddForm(redirect='/artis/get_all').req()
         return dict(widget=w, page='add page')
-        #redirect('/artist/get_all')
     
     @expose()
     @validate(add_form, error_handler=add)
     def post_add(self, firstname='', lastname='', sitelink='', reellink='',
                     role='', software='', tags='', phone='', email='', skype='', othercontacts='',
-                    note='', newartist=''):
+                    note='', newartist='', cv_upload=''):
         print (firstname, lastname, sitelink, reellink, role, software, tags, phone, email, skype,
                 othercontacts, note, newartist)
         
         artist = Artist(firstname=firstname, lastname=lastname, sitelink=sitelink, reellink=reellink, 
                     note = note, newartist = newartist, othercontacts = othercontacts)
+        
+        if cv_upload != '':
+            ext = shutil.os.path.splitext(cv_upload.filename)[1]
+            dest_path = config['cv_repo']
+            dest_path = shutil.os.path.join(dest_path,"%s_%s%s" % (firstname, lastname, ext))
+            f = open(dest_path, 'wb')
+            f.write(cv_upload.file.read())
+            f.close()
+            artist.cvlocal = ("%s_%s%s" % (firstname, lastname, ext))
+            print (url(artist.cvlocal))
+            print (url('/ds'))
         
         data_list = helpers.get_list_from_string(role)
         for d in data_list:
@@ -140,6 +155,19 @@ class Controller(BaseController):
         transaction.commit()
         redirect('/artist/get_all')
         
+    @expose()
+    def download(self, file_name):
+    
+        f = shutil.os.path.join(config['cv_repo'], file_name)
         
+        # set the correct content-type so the browser will know what to do
+        content_type, encoding = mimetypes.guess_type(path)
+        response.headers['Content-Type'] = content_type
+        response.headers['Content-Disposition'] = (
+                                        ('attachment; filename=%s' %
+                                            file_name).encode())
         
-        
+        # copy file content in the response body
+        shutil.copyfileobj(f, response.body_file)
+        f.close()
+        return
